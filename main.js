97ec8cafa8794e54ad6852aedfa5f4a86f7ba9b1
Immigrate to Brazil <immigratetobrazilteam@email.com>
2026-01-04 11:07:53 -0300

    Initial commit for Tocantins

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("/partials/header.html")
         .then(r => r.text())
         .then(html => document.body.insertAdjacentHTML("afterbegin", html));
-
     fetch("/partials/footer.html")
         .then(r => r.text())
         .then(html => document.body.insertAdjacentHTML("beforeend", html));
